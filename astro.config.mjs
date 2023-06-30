6617f48852f859db8983137f7bf54e13b6f225b5
kubawajs <jakub.wajs10@gmail.com>
2023-06-30 23:37:49 +0200

    Add robots, sitemap and compression to the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,34 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
 import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
+const domainUrl = "https://www.wajs-dev.net";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kubawajs.github.io',
+  site: domainUrl,
   integrations: [
     image(),
+    compress(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
-      }
-    })
-  ]
+      },
+    }),
+    sitemap({
+      filter: (page) => page.toLowerCase().indexOf(`${domainUrl}/work`) === -1,
+    }),
+    robotsTxt({
+      sitemap: `${domainUrl}/sitemap-index.xml`,
+      policy: [
+        {
+          userAgent: '*',
+          disallow: ["/work", "/work/*"],
+        },
+      ],
+    }),
+  ],
 });
